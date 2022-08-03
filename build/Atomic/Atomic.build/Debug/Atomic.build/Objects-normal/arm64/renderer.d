c7bae407e559835e7e6c49adb88cbc6511adb54c
5aitama <alx.sb@icloud.com>
2022-08-03 13:36:50 +0200

    implement the material functions

--- build/Atomic/Atomic.build/Debug/Atomic.build/Objects-normal/arm64/renderer.d
+++
@@ -1,8 +0,0 @@
-dependencies: /Users/alex/Desktop/Atomic/Atomic/renderer/renderer.c \
-  /Users/alex/Desktop/Atomic/Atomic/renderer/renderer.h \
-  /Users/alex/Desktop/Atomic/Atomic/renderer/../surface/surface.h \
-  /Users/alex/Desktop/Atomic/Atomic/renderer/../surface/../Atomic.h \
-  /Users/alex/Desktop/Atomic/modules/glfw/include/GLFW/glfw3.h \
-  /Users/alex/Desktop/Atomic/modules/glfw/include/GLFW/glfw3native.h \
-  /Users/alex/Desktop/Atomic/modules/wgpu/ffi/wgpu.h \
-  /Users/alex/Desktop/Atomic/modules/wgpu/ffi/webgpu-headers/webgpu.h
--- build/Atomic/Atomic.build/Debug/Atomic.build/Objects-normal/arm64/surface.d
+++
@@ -1,7 +0,0 @@
-dependencies: /Users/alex/Desktop/Atomic/Atomic/surface/surface.c \
-  /Users/alex/Desktop/Atomic/Atomic/surface/surface.h \
-  /Users/alex/Desktop/Atomic/Atomic/surface/../Atomic.h \
-  /Users/alex/Desktop/Atomic/modules/glfw/include/GLFW/glfw3.h \
-  /Users/alex/Desktop/Atomic/modules/glfw/include/GLFW/glfw3native.h \
-  /Users/alex/Desktop/Atomic/modules/wgpu/ffi/wgpu.h \
-  /Users/alex/Desktop/Atomic/modules/wgpu/ffi/webgpu-headers/webgpu.h
